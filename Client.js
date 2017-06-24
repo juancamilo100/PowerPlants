4b0f2b40bd24facebcb4088e6e4f75c89ec0527a
David Jackson <david.jackson@turner.com>
2017-06-24 11:50:16 -0400

    Extrapolated outlierIdentifier

--- Client.js
+++ Client.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = "http://ascbackend.azurewebsites.net/api/powertrip/plants/86a703c1497440eea0c92fc35a44184e"
+var outlierIdentifier = require(outlierIdentifier);
 
 var request = http.get(url, function(response) {
     console.dir(response.statusCode);
@@ -14,40 +15,7 @@ var request = http.get(url, function(response) {
         try {
           var parsedData = JSON.parse(body);
           console.log(parsedData)
-
-          var plants = []
-          for (var i in parsedData)
-          {
-            var plant = parsedData[i].plant
-
-            var deliveredArray = plant.delivered
-            for (var i = 0; i < deliveredArray; i++) {
-              if (deliveredArray[i].time.isValid()) // TODO
-                plant.dtSum += deliveredArray[i].time
-            }
-
-            plants.push(plant)
-          }
-
-          console.log("plants (pre-sort): " + plants)
-
-          plants.sort(function(a, b) {
-            return a - b;
-          })
-
-          console.log("plants (post-sort): " + plants)
-
-          var outlier;
-          if ((plants[1].dtSum - plants[0].dtSum) > (plants[plants.length - 1].dtSum - plants[plants.length - 2].dtSum)) {
-            outlier = plants[0]
-          }
-          else {
-            outlier = plants[plants.length - 1]
-          }
-
-          console.log("outlier: " + outlier)
-
-          // return outlier.id
+          var outlierId = outlierIdentifier.findOutlierId(parsedData)
         } catch(error) {
           console.log(error);
         }
